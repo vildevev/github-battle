509e1da29b2804c415188b606a139a54bbdefdfd
“Vilde <vilde@vevatne.no>
2017-07-13 15:03:37 -0700

    Add componentdidmount function to popular component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,5 +1,6 @@
 var React = require('react'); 
 var PropTypes = require('prop-types');
+var api = require('../utils/api'); 
 
 function SelectLanguage (props) {
 	var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -28,11 +29,20 @@ class Popular extends React.Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			selectedLanguage: 'All'
+			selectedLanguage: 'All',
+			repos: null
 		};
 
 		this.updateLanguage = this.updateLanguage.bind(this); 
 	}
+
+	componentDidMount() {
+		api.fetchPopularRepos(this.state.selectedLanguage)
+			.then(function (repos) {
+				console.log(repos)
+			})
+	}
+
 	updateLanguage(lang) {
 		this.setState(function () {
 			return {
