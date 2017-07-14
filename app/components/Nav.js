8c89276cc6e5bfde7a74364309860d73d1a05318
“Vilde <vilde@vevatne.no>
2017-07-13 17:48:04 -0700

    Add navbar

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,7 +1,26 @@
 var React = require('react');
+var NavLink = require('react-router-dom').NavLink; 
 
 function Nav () {
-
+	return (
+		<ul className='nav'>
+			<li>
+				<NavLink exact activeClassName='active' to='/'>
+					Home
+				</NavLink>
+			</li>
+			<li>
+				<NavLink activeClassName='active' to='/popular'>
+					Popular
+				</NavLink>
+			</li>
+			<li>
+				<NavLink activeClassName='active' to='/battle'>
+					Battle
+				</NavLink>
+			</li>
+		</ul>
+	)
 }
 
 module.exports = Nav; 
