c2ab98ec28bf8a8739189f61458cbc465428206d
“Vilde <vilde@vevatne.no>
2017-07-16 08:30:36 -0700

    Add github api request functions

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,45 @@
 var axios = require('axios');
 
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id" + id + "&client_secret=" + sec; 
+
+function getProfile (username) {
+	return axios.get('https://api.github.com/users/' + username + params)
+		.then(function (user) {
+			return user.data;
+		});
+}
+
+function getRepos (username) {
+	return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+function getStarCount (repos) {
+	return repos.data.reduce(function (count, repo) {
+		return count + repo.stargazers_count; 
+	}, 0)
+}
+
+function handleError (error) {
+	console.warn(error);
+	return null;
+}
+
+function getUserData (player) {
+	return axios
+}
+
+function calculateScore (profile, repos) {
+	var followers = profile.followers;
+	var totalStars = getStarCount(repos);
+
+	return (followers * 3) + totalStars;
+}
 module.exports = {
+	battle: function (players) {
+
+	},
 	fetchPopularRepos: function (language) {
 		var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
 
